bb81b719c88b0f7e67eefdfc35a5484af1849e8a
Justin <kerrj@guilford.edu>
2016-03-02 14:13:37 -0500

    Push by Justin Kerr on Wednesday March 2nd. Changes: 1. Finished layout of titlepage, with background color and title text/buttons 2. Added ECG logo to title page

--- app/src/main/java/com/weebly/helloworldclub/phoenixnow/MainActivity.java
+++ app/src/main/java/com/weebly/helloworldclub/phoenixnow/MainActivity.java
@@ -1,9 +1,11 @@
 package com.weebly.helloworldclub.phoenixnow;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     @Override
