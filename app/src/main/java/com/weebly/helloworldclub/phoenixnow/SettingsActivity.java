44fbc32e588ecbd676827850f5158574f4a0310e
kerrj <kerrj@guilford.edu>
2016-05-23 20:36:19 -0400

    added credits

--- app/src/main/java/com/weebly/helloworldclub/phoenixnow/SettingsActivity.java
+++ app/src/main/java/com/weebly/helloworldclub/phoenixnow/SettingsActivity.java
@@ -23,17 +23,17 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedinstancestance);
         setContentView(R.layout.settings);
         File settings=new File(getApplicationContext().getFilesDir().getPath()+"/settings.txt");
-        Switch switch1=(Switch)findViewById(R.id.switch1);
+        Switch rememberLogin=(Switch)findViewById(R.id.rememberLogin);
             try {
                 FileInputStream fis = new FileInputStream(settings);
                 byte[] buffer=new byte[fis.available()];
                 fis.read(buffer);
                 fis.close();
                 String string=new String(buffer,"UTF8");
-                if(string.contains("switch1:on")){
-                    switch1.setChecked(true);
+                if(string.contains("rememberLogin:on")){
+                    rememberLogin.setChecked(true);
                 }else{
-                    switch1.setChecked(false);
+                    rememberLogin.setChecked(false);
                 }
                 //set switches on or off based on memory here
             } catch (FileNotFoundException e) {
@@ -41,36 +41,36 @@ public class SettingsActivity extends AppCompatActivity {
             }catch(java.io.IOException e){
                 e.printStackTrace();
             }
-        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        rememberLogin.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 try {
-                    File settings=new File(getApplicationContext().getFilesDir().getPath()+"/settings.txt");
-                    FileInputStream fis=new FileInputStream(settings);
-                    byte[] buffer=new byte[fis.available()];
+                    File settings = new File(getApplicationContext().getFilesDir().getPath() + "/settings.txt");
+                    FileInputStream fis = new FileInputStream(settings);
+                    byte[] buffer = new byte[fis.available()];
                     fis.read(buffer);
                     fis.close();
-                    String string=new String(buffer,"UTF8");
+                    String string = new String(buffer, "UTF8");
                     Log.d("settings initial", string);
-                    if(string.contains("switch1:on")) {
-                        string=string.replace("switch1:on", "switch1:off");
-                    }else{
-                        string=string.replace("switch1:off", "switch1:on");
+                    if (string.contains("rememberLogin:on")) {
+                        string = string.replace("rememberLogin:on", "rememberLogin:off");
+                    } else {
+                        string = string.replace("rememberLogin:off", "rememberLogin:on");
                     }
-                    Log.d("settings final",string);
-                    FileOutputStream fos=new FileOutputStream(settings);
+                    Log.d("settings final", string);
+                    FileOutputStream fos = new FileOutputStream(settings);
                     fos.write(string.getBytes());
                     fos.close();
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
-                }catch(java.io.IOException e){
+                } catch (java.io.IOException e) {
                     e.printStackTrace();
                 }
             }
         });
         //add more listeners with same format as above for other switches
     }
-    public boolean getSwitch1(Context context){
+    public boolean getRememberLogin(Context context){
         File settings=new File(context.getApplicationContext().getFilesDir().getPath()+"/settings.txt");
         if(settings.exists()) {
             try {
@@ -79,7 +79,7 @@ public class SettingsActivity extends AppCompatActivity {
                 fis.read(buffer);
                 fis.close();
                 String string = new String(buffer, "UTF8");
-                if (string.contains("switch1:on")) {
+                if (string.contains("rememberLogin:on")) {
                     return true;
                 } else {
                     return false;
@@ -100,7 +100,7 @@ public class SettingsActivity extends AppCompatActivity {
                 switch1.setChecked(false);//initialize switches here
                 settings.createNewFile();
                 FileOutputStream fos=new FileOutputStream(settings);
-                String string="switch1:off ";//and here
+                String string="rememberLogin:off ";//and here
                 fos.write(string.getBytes());
                 fos.close();
             } catch (IOException e) {
