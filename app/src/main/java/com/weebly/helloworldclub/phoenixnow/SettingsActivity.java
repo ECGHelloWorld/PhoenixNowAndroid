bb58bd34388dd04f275b7efe169215161ce4b14f
Justin <kerrj@guilford.edu>
2016-08-19 13:14:26 -0400

    removed "remember login" setting from settings, updated credits

--- app/src/main/java/com/weebly/helloworldclub/phoenixnow/SettingsActivity.java
+++ app/src/main/java/com/weebly/helloworldclub/phoenixnow/SettingsActivity.java
@@ -23,84 +23,29 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedinstancestance);
         setContentView(R.layout.settings);
         File settings=new File(getApplicationContext().getFilesDir().getPath()+"/settings.txt");
-        Switch rememberLogin=(Switch)findViewById(R.id.rememberLogin);
             try {
                 FileInputStream fis = new FileInputStream(settings);
                 byte[] buffer=new byte[fis.available()];
                 fis.read(buffer);
                 fis.close();
                 String string=new String(buffer,"UTF8");
-                if(string.contains("rememberLogin:on")){
-                    rememberLogin.setChecked(true);
-                }else{
-                    rememberLogin.setChecked(false);
-                }
                 //set switches on or off based on memory here
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }catch(java.io.IOException e){
                 e.printStackTrace();
             }
-        rememberLogin.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                try {
-                    File settings = new File(getApplicationContext().getFilesDir().getPath() + "/settings.txt");
-                    FileInputStream fis = new FileInputStream(settings);
-                    byte[] buffer = new byte[fis.available()];
-                    fis.read(buffer);
-                    fis.close();
-                    String string = new String(buffer, "UTF8");
-                    Log.d("settings initial", string);
-                    if (string.contains("rememberLogin:on")) {
-                        string = string.replace("rememberLogin:on", "rememberLogin:off");
-                    } else {
-                        string = string.replace("rememberLogin:off", "rememberLogin:on");
-                    }
-                    Log.d("settings final", string);
-                    FileOutputStream fos = new FileOutputStream(settings);
-                    fos.write(string.getBytes());
-                    fos.close();
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                } catch (java.io.IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        //add more listeners with same format as above for other switches
-    }
-    public boolean getRememberLogin(Context context){
-        File settings=new File(context.getApplicationContext().getFilesDir().getPath()+"/settings.txt");
-        if(settings.exists()) {
-            try {
-                FileInputStream fis = new FileInputStream(settings);
-                byte[] buffer = new byte[fis.available()];
-                fis.read(buffer);
-                fis.close();
-                String string = new String(buffer, "UTF8");
-                if (string.contains("rememberLogin:on")) {
-                    return true;
-                } else {
-                    return false;
-                }
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (java.io.IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return false;
+        //add listeners to handle switches
     }
     public void initializeSettings(Context context){
         File settings=new File(context.getApplicationContext().getFilesDir().getPath()+"/settings.txt");
         if(!settings.exists()) {
             try {
                 settings.createNewFile();
-                FileOutputStream fos=new FileOutputStream(settings);
-                String string="rememberLogin:off ";//initialize switches
-                fos.write(string.getBytes());
-                fos.close();
+//                FileOutputStream fos=new FileOutputStream(settings);
+//                String string="rememberLogin:off ";//initialize switches
+//                fos.write(string.getBytes());
+//                fos.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
