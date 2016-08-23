419865fc2cd52b99ca5c54c3d28ea708d4f703fc
Varun-Nair <nairvarun18@gmail.com>
2016-08-23 01:22:02 -0400

    Started to setup password reset

---
+++ app/src/main/java/com/weebly/helloworldclub/phoenixnow/RequestResetActivity.java
@@ -0,0 +1,17 @@
+package com.weebly.helloworldclub.phoenixnow;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by nairv on 8/23/2016.
+ */
+public class RequestResetActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
